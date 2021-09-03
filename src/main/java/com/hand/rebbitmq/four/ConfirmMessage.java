5ea5a8a5ee7e09e4e2cec8ee9a8d30b395aae691
zhangzhuop <1204833168@qq.com>
2021-09-03 16:02:32 +0800

    异步发布确认

--- src/main/java/com/hand/rebbitmq/four/ConfirmMessage.java
+++ src/main/java/com/hand/rebbitmq/four/ConfirmMessage.java
@@ -2,6 +2,7 @@ package com.hand.rebbitmq.four;
 
 import com.hand.rebbitmq.utils.RabbitUtils;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -20,11 +21,12 @@ public class ConfirmMessage {
     public static final int MESSAGE_COUNT=1000;
     public static void main(String[] args) throws Exception {
 
-        //1.单个确认
+        //1.单个确认   发布1000个单独确认消息，耗时:15794ms
 //        publishMessgaeOne();
-        //2.批量确认
-        publishMessageBatch();
-        //3.异步批量确认
+        //2.批量确认  发布1000个批量确认消息，耗时:556ms
+//        publishMessageBatch();
+        //3.异步批量确认   发布1000个异步确认消息，耗时:38ms
+//        publishMessageAsync();
     }
 
     public static void publishMessgaeOne() throws Exception {
@@ -79,5 +81,45 @@ public class ConfirmMessage {
         //结束时间
         long end = System.currentTimeMillis();
         System.out.println("发布"+MESSAGE_COUNT+"个批量确认消息，耗时:"+(end-begin)+"ms");
+    }
+
+    //异步发布确认
+    public static void publishMessageAsync() throws Exception{
+        Channel channel = RabbitUtils.getChannel();
+        //声明队列
+        String queueName = UUID.randomUUID().toString();
+        channel.queueDeclare(queueName,false,false,false,null);
+        //开启发布确认
+        channel.confirmSelect();
+        //开始时间
+        long begin = System.currentTimeMillis();
+        //消息确认成功回调函数
+        ConfirmCallback ackCallback = (deliveryTag,multiple) ->{
+            System.out.println("确认的消息:"+deliveryTag);
+        };
+        //消息确认失败回调函数
+        /**
+         * deliveryTag 消息的标记
+         * multiple 是否为批量确认
+         */
+        ConfirmCallback nackCallback = (deliveryTag,multiple) ->{
+            System.out.println("未确认的消息:"+deliveryTag);
+        };
+        //准备消息的监听器 监听哪些消息成功了 哪些消息失败了
+        /**
+         * 异步通知
+         * 1.监听哪些消息成功
+         * 2.监听哪些消息失败
+         */
+        channel.addConfirmListener(ackCallback,nackCallback);
+
+        for(int i=0;i<MESSAGE_COUNT;i++) {
+            String message = "消息:" + i;
+            channel.basicPublish("", queueName, null, message.getBytes(StandardCharsets.UTF_8));
+        }
+
+        //结束时间
+        long end = System.currentTimeMillis();
+        System.out.println("发布"+MESSAGE_COUNT+"个异步确认消息，耗时:"+(end-begin)+"ms");
     }
 }
