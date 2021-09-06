32fa4d77aeac644faa56508dfea244bc99eb1001
zhangzhuop <1204833168@qq.com>
2021-09-06 14:23:27 +0800

    hello world

--- src/main/java/com/hand/rebbitmq/one/Producer.java
+++ src/main/java/com/hand/rebbitmq/one/Producer.java
@@ -36,7 +36,7 @@ public class Producer {
          * 1.队列名称
          * 2.队列里的消息是否持久化
          * 3.队列是否只供一个消费者进行消费 是否进行消息共享，true可以多个消费者消费
-         * 4.是否自动删除  最后一个消费者断开连接以后队列是否自动删除
+         * 4.是否自动删除   最后一个消费者断开连接以后队列是否自动删除
          * 5.其他参数
          */
         channel.queueDeclare(QUEUE_NAME,false,false,false,null);
